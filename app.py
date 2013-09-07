f3641335e110dfcb5ffce0c30a1bd6d2cc001872
Avi Romanoff <aviromanoff@gmail.com>
2013-09-07 13:25:24 -0400

    More sane user auth

--- app.py
+++ app.py
@@ -49,13 +49,12 @@ def create_meal(name):
 def setup():
     oauth_code = request.args.get('code')
     if oauth_code:
+        url = "https://api.venmo.com/oauth/access_token"
         data = {
             "client_id": VENMO_OAUTH_CLIENT_ID,
             "client_secret": VENMO_OAUTH_CLIENT_SECRET,
             "code": oauth_code
         }
-
-        url = "https://api.venmo.com/oauth/access_token"
         response = requests.post(url, data)
         response_dict = response.json()
         error = response_dict.get('error')
@@ -63,18 +62,38 @@ def setup():
             return "Error from Venmo OAUTH: %s" % error
         access_token = response_dict.get('access_token')
         user = response_dict.get('user')
+
+        print "User from venmo oauth:"
+        pp(user)
+        user_from_db = mongo.db.users.find_one({"venmo_id": user['id']})
+
+        print "User from db:"
+        pp(user_from_db)
+
+        if user_from_db:
+            print "User has used GrubHero before; we have them in the DB."
+            user = user_from_db
+            mongo.db.users.update({"venmo_id": user['id']}, {
+                "firstname": user['firstname'],
+                "lastname": user['lastname'],
+                "picture": user['picture'],
+                "last_visit": datetime.datetime.utcnow(),
+            })
+        else:
+            print "User has NOT used GrubHero before. Making account in DB."
+            user = {
+                "venmo_id": user['id'],
+                "access_token": access_token,
+                "firstname": user['firstname'],
+                "lastname": user['lastname'],
+                "picture": user['picture'],
+                "last_visit": datetime.datetime.utcnow()
+            }
+            mongo.db.users.update({"venmo_id": user['id'], })
+
         session['venmo_id'] = user['id']
         session['firstname'] = user['firstname']
         session['lastname'] = user['lastname']
-        user = {
-            "venmo_id": user['id'],
-            "access_token": access_token,
-            "firstname": user['firstname'],
-            "lastname": user['lastname'],
-            "picture": user['picture'],
-            "last_visit": datetime.datetime.utcnow()
-        }
-        mongo.db.users.insert(user)
         return redirect(url_for('index'))
     else:
         return "Error"
