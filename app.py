30b0cd13a010237f7ebf9172d080b7880515d8a0
Avi Romanoff <aviromanoff@gmail.com>
2013-09-07 02:02:58 -0400

    rebrand to MealsOnWheels

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask
+from flask_oauth import OAuth
+
 app = Flask(__name__)
 
-APP_NAME = "venmo-food"
+VENMO_ACCESS_TOKEN = "eSN3Z3A2KeRbcnNTqgLu6mRA4K9uED9V"
+
+oauth = OAuth()
+venmo = oauth.remote_app('venmo',
+	base_url = 'https://sandbox-api.venmo.com/',
+	request_token_url = 'https://api.venmo.com'
+)
 
 @app.route("/")
 def hello():
-    return "Welcome to %s!" % APP_NAME
+    return "Welcome to GrubHero, you are on the homepage."
+
+@app.route
 
 if __name__ == "__main__":
+	app.debug = True
     app.run()
