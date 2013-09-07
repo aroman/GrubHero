a45e6b306d0eb6e292c41f9e9dcf7121e022ff32
Avi Romanoff <aviromanoff@gmail.com>
2013-09-07 01:17:19 -0400

    lasodas

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-# This is the app server.
+from flask import Flask
+app = Flask(__name__)
+
+APP_NAME = "venmo-food"
+
+@app.route("/")
+def hello():
+    return "Welcome to %s!" % APP_NAME
+
+if __name__ == "__main__":
+    app.run()
